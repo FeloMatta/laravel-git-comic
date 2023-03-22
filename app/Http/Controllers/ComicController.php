9e662fcf6083b8c2c0b551ba92ea9b054cff7e90
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-03-22 17:45:47 +0100

    implementazione store

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -35,7 +35,20 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+
+        $newComic = new Comic;
+        $newComic->title= $data['title'];
+        $newComic->description=$data['description'];
+        $newComic->image=null;
+        $newComic->price=$data['price'];
+        $newComic->series=$data['series'];
+        $newComic->sale_date=null;
+        $newComic->type=$data['type'];
+        $newComic->save();
+
+        return redirect()->route('comics.index');
     }
 
     /**
